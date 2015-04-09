a2e973e83719f5dd10b78155f2378872d64fe97b
MrMM1989 <maarten_89@msn.com>
2015-04-10 00:38:25 +0200

    Basic HTML Template setup version 1
    Added a reset stylesheet, base CSS stylesheet and the start of the basic
    of the pages.

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -13,6 +13,7 @@ class Home extends CI_Controller {
 	 */
 	public function index()
 	{
-		$this->load->view('welcome_message');
+		$this->load->view('header');
+		$this->load->view('footer');
 	}
 }
