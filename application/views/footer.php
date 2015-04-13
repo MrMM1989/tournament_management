7ef68de1fb5b56ae277b15ea9b603828ac5b00ba
MrMM1989 <maarten_89@msn.com>
2015-04-14 01:33:28 +0200

    Front-end setup
    Added the necessary links for the base front-end script

--- application/views/footer.php
+++ application/views/footer.php
@@ -5,5 +5,8 @@
 		<footer>
 			
 		</footer>
+		<script src="//code.jquery.com/jquery-1.11.2.min.js"></script>
+		<script src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+		<script src="<?php echo base_url('public/js/base.js'); ?>"></script>
  	</body>
 </html>
---
+++ public/js/base.js
@@ -0,0 +1,9 @@
+/*
+ * Document ready function 
+ * 
+ * Calls all the necessary functions and objects when the document is ready
+ */
+
+$( document ).ready(function() {
+  alert('Koppeling gelukt! Yeehaa!');
+});
